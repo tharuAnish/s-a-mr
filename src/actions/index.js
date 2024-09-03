a80a0224568b828921fcae9ab42dd89403817ec9
tharuAnish <namoanishtharu@gmail.com>
2024-09-03 17:02:18 +0530

    get current user

--- src/actions/index.js
+++ src/actions/index.js
@@ -101,3 +101,39 @@ export async function loginUserAction(formData) {
     }
   }
 }
+
+//Fetch User info
+export async function fetchAuthUserAction() {
+  await connectToDB()
+  try {
+    const getCookies = cookies()
+    const token = getCookies.get("token")?.value || ""
+    if (token === "") {
+      return {
+        success: false,
+        message: "Token is invalid",
+      }
+    }
+
+    const decodedToken = jwt.verify(token, "DEFAULT_KEY")
+    const getUserInfo = await User.findOne({ _id: decodedToken.id })
+
+    if (getUserInfo) {
+      return {
+        success: true,
+        data: JSON.parse(JSON.stringify(getUserInfo)),
+      }
+    } else {
+      return {
+        success: false,
+        message: "Some error occured ! Please try again",
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      success: false,
+      message: "Something went wrong! please try again",
+    }
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
-export default function Home() {
+import { fetchAuthUserAction } from "@/actions"
+
+export default async function Home() {
+  const currentUser = await fetchAuthUserAction()
+  console.log(currentUser)
+
   return (
     <div>
       <h1>NextJs Authentication</h1>
+      <h2>{currentUser?.data.userName}</h2>
+      <p>{currentUser?.data.email}</p>
     </div>
   )
 }
