84d1492a1721dd93480c72c738b0771decc587b6
tharuAnish <namoanishtharu@gmail.com>
2024-09-03 10:32:23 +0530

    created form wit form controls

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,5 +1,36 @@
 "use client"
 
-export default function SignIn() {
-  return <div>SignIn</div>
+import { useState } from "react"
+
+import { Form } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { initialSignupFormData, userRegestrationFormControls } from "../utils"
+import CommonFormElement from "@/components/form-element/page"
+
+export default function SignUp() {
+  const [signupFormData, setSignupFormdata] = useState(initialSignupFormData)
+  console.log(signupFormData)
+  return (
+    <div>
+      <h1>Regestration</h1>
+      <Form>
+        {userRegestrationFormControls.map((controlItem) => (
+          <div key={controlItem.name}>
+            <Label>{controlItem.label}</Label>
+            <CommonFormElement
+              value={signupFormData[controlItem.name]}
+              currentItem={controlItem}
+              onChange={(event) =>
+                setSignupFormdata({
+                  ...signupFormData,
+                  [event.target.name]: event.target.value,
+                })
+              }
+            />
+          </div>
+        ))}
+      </Form>
+    </div>
+  )
 }
---
+++ src/app/utils/index.js
@@ -0,0 +1,29 @@
+export const userRegestrationFormControls = [
+  {
+    name: "userName",
+    label: "User Name",
+    placeholder: "Enter your username",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    componentType: "input",
+    type: "password",
+  },
+]
+
+export const initialSignupFormData = {
+  userName: "",
+  email: "",
+  password: "",
+}
